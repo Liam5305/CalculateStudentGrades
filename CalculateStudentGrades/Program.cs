422ab66e506250df719dc6751f621624f264a483
Liryls <liamrutherford10@gmail.com>
2024-10-03 16:20:38 +0100

    Add project files.

---
+++ CalculateStudentGrades/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StudentGrades();
+    }
+
+    static void StudentGrades()
+    {
+        List<String> studentNames = new List<String>();
+        List<String> studentGrades = new List<String>();
+
+        while (true)
+        {
+            Console.WriteLine("Please enter a Student's name");
+        }
+    }
+}
